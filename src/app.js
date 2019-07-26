f8f11aea58c1eaefeae3c0bfd4e4a117d5ac559c
oluwashane <oluwashane@gmail.com>
2019-07-26 11:15:26 +0100

    Fixed error in port

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
-const port = process.env.PORT ¦¦ 3000;
+const port = process.env.PORT || 3000;
 
 // Define path for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
